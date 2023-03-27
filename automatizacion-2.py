57255bbb8a7c47030c853ee2ddeec3bdf5854761
rai <ravaen1001@gmail.com>
2023-03-26 23:10:11 -0500

    cambios en salidas de columnas

--- automatizacion-2.py
+++ automatizacion-2.py
@@ -6,7 +6,7 @@ tabla_b = pd.read_excel('tabla_b.xlsx', sheet_name="Hoja1")
 
 # Realizar la operación de búsqueda y combinación
 tabla_b = tabla_b.merge(tabla_a[['SUMINISTRO', 'NOMBRES', 'NUMEROFICHA']], on='SUMINISTRO', how='left')
-tabla_b['NUMEROFICHA'] = tabla_b['NUMEROFICHA'].fillna('').astype(str) + tabla_b['NOMBRES'].fillna('').astype(str)
+tabla_b['NUMEROFICHA'] = tabla_b['NUMEROFICHA'].fillna('') + tabla_b['NOMBRES'].fillna('')
 
 # Guardar el resultado en un nuevo archivo de Excel
-tabla_b.to_excel('tabla_b.xlsx', index=False)
+tabla_b.to_excel('tabla_b.xlsx', index=False) 
