094cb99a1ea2b510d5990eeb73b1117167714b03
rai <ravaen1001@gmail.com>
2023-03-27 00:04:13 -0500

    siguen modificaciones en salidas de campos

--- automatizacion-2.py
+++ automatizacion-2.py
@@ -5,8 +5,7 @@ tabla_a = pd.read_excel('tabla_a.xlsx', sheet_name="Hoja1")
 tabla_b = pd.read_excel('tabla_b.xlsx', sheet_name="Hoja1")
 
 # Realizar la operación de búsqueda y combinación
-tabla_b = tabla_b.merge(tabla_a[['SUMINISTRO', 'NOMBRES', 'NUMEROFICHA']], on='SUMINISTRO', how='left')
-tabla_b['NUMEROFICHA'] = tabla_b['NUMEROFICHA'].fillna('') + tabla_b['NOMBRES'].fillna('')
+tabla_b = tabla_b.merge(tabla_a[['SUMINISTRO', 'NUMEROFICHA']], on='SUMINISTRO', how='left')
 
 # Guardar el resultado en un nuevo archivo de Excel
 tabla_b.to_excel('tabla_b.xlsx', index=False) 
